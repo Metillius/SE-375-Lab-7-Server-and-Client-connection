abeee8cf86577c531a791d1b8c526f2aff34a08e
Arda <85706449+Metillius@users.noreply.github.com>
2023-05-03 01:07:15 +0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -11,7 +11,7 @@ public class Client{
 
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
-        Socket clientSocket = new Socket("localhost", 193);
+        Socket clientSocket = new Socket("localhost", 6789);
 
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -20,8 +20,15 @@ public class Client{
 
 
         outToServer.writeBytes("Balcova.csv,Bornova.csv,Gaziemir.csv,Karabaglar.csv,Karsiyaka.csv,Konak.csv,Seferihisar.csv,Urla.csv" +'\n');
+        outToServer.flush();
         modifiedSentence = inFromServer.readLine();
-        System.out.println("FROM SERVER: " + modifiedSentence);
+        System.out.println( modifiedSentence);
+        String selection = inFromUser.readLine();
+        outToServer.writeBytes(selection + '\n');
+        String answer= inFromServer.readLine();
+        System.out.println(answer);
+
+
 
 
 
@@ -29,4 +36,5 @@ public class Client{
     }
 
 }
+
 

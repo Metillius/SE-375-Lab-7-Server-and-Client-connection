9c1a9310445bf9b97fb2b6608c9998d01268fa04
Arda <85706449+Metillius@users.noreply.github.com>
2023-05-03 13:55:55 +0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Client{
     public static void main(String argv[]) throws Exception {
@@ -19,23 +20,68 @@ public class Client{
         System.out.println("Connection established");
 
 
+        ArrayList<String> destinationList = new ArrayList<>();
+        destinationList.add("Balcova");
+        destinationList.add("Bornova");
+        destinationList.add("Gaziemir");
+        destinationList.add("Karabaglar");
+        destinationList.add("Karsiyaka");
+        destinationList.add("Konak");
+        destinationList.add("Balcova");
+        destinationList.add("Seferihisar");
+        destinationList.add("Urla");
+
 
         outToServer.writeBytes("Balcova.csv,Bornova.csv,Gaziemir.csv,Karabaglar.csv,Karsiyaka.csv,Konak.csv,Seferihisar.csv,Urla.csv" +'\n');
         outToServer.flush();
         modifiedSentence = inFromServer.readLine();
+
         System.out.println( modifiedSentence);
         String selection = inFromUser.readLine();
-        outToServer.writeBytes(selection + '\n');
-        String answer= inFromServer.readLine();
-        System.out.println(answer);
+        while(!selection.equals("exit") ){
 
-        String ThreadAnswer = inFromServer.readLine();
-        String ThreadAnswer2 = inFromServer.readLine();
-        String ThreadAnswer3 = inFromServer.readLine();
-        System.out.println(ThreadAnswer);
-        System.out.println(ThreadAnswer2);
-        System.out.println(ThreadAnswer3);
 
+            outToServer.writeBytes(selection + '\n');
+            String answer= inFromServer.readLine();
+            System.out.println(answer);
+
+
+
+            if(destinationList.contains(selection)){
+
+                String ThreadAnswer = inFromServer.readLine();
+                String ThreadAnswer2 = inFromServer.readLine();
+                String ThreadAnswer3 = inFromServer.readLine();
+
+                System.out.println(ThreadAnswer);
+                System.out.println(ThreadAnswer2);
+                System.out.println(ThreadAnswer3);
+            }
+
+            else {
+                System.out.println("Invalid entry");
+            }
+
+
+
+            modifiedSentence = inFromServer.readLine();
+
+            System.out.println( modifiedSentence);
+            selection = inFromUser.readLine();
+
+
+
+
+        }
+        if (modifiedSentence == "exit"){
+            System.out.println("Thank you for using the system...");
+        }
+
+    /*    else{
+            System.out.println("Invalid entry");
+        }
+
+     */
 
 
 
